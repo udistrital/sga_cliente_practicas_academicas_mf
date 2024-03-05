44d7b309e007bcd255f4c6839359e0f9a34c8969
bfmunozm <swsbmm@gmail.com>
2024-03-05 12:19:23 -0500

    feat: udistrital/sga_documentacion#66 correccion de variables de entorno para produccion

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,13 +1,12 @@
 export const environment = {
   production: true,
   urlMf: "http://localhost:4210/",
-  //NUXEO_SERVICE:'https://autenticacion.portaloas.udistrital.edu.co/apioas/gestor_documental_mid/v1',
-  NUXEO_SERVICE: "http://pruebasapi2.intranetoas.udistrital.edu.co:8199/v1",
+  NUXEO_SERVICE:
+    "https://autenticacion.portaloas.udistrital.edu.co/apioas/gestor_documental_mid/v1",
   TERCEROS_SERVICE:
     "https://autenticacion.portaloas.udistrital.edu.co/apioas/terceros_crud/v1/",
-  //TERCEROS_SERVICE: 'http://pruebasapi.intranetoas.udistrital.edu.co:8121/v1/',
-  //SGA_MID_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/sga_mid/v1/',
-  //SGA_MID_SERVICE: 'http://localhost:8119/v1/',
-  SGA_MID_SERVICE: "http://pruebasapi.intranetoas.udistrital.edu.co:8119/v1/",
-  DOCUMENTO_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/documento_crud/v2/',
+  SGA_MID_SERVICE:
+    "https://autenticacion.portaloas.udistrital.edu.co/apioas/sga_mid/v1/",
+  DOCUMENTO_SERVICE:
+    "https://autenticacion.portaloas.udistrital.edu.co/apioas/documento_crud/v2/",
 };
