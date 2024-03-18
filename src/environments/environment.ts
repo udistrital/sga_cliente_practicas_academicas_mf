862ba12d2c3bb6c829703ff30bb9fbd8e08f6003
bfmunozm <swsbmm@gmail.com>
2024-03-18 12:01:21 -0500

    feat: Actualiza las URL de los servicios en el archivo environment.ts udistrital/sga_documentacion#106

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,8 @@
 export const environment = {
   production: false,
   urlMf: "http://localhost:4210/",
-  //NUXEO_SERVICE:'https://autenticacion.portaloas.udistrital.edu.co/apioas/gestor_documental_mid/v1',
   NUXEO_SERVICE: "http://pruebasapi2.intranetoas.udistrital.edu.co:8199/v1",
-  //TERCEROS_SERVICE:
-  //  "https://autenticacion.portaloas.udistrital.edu.co/apioas/terceros_crud/v1/",
   TERCEROS_SERVICE: 'http://pruebasapi.intranetoas.udistrital.edu.co:8121/v1/',
-  //SGA_MID_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/sga_mid/v1/',
-  //SGA_MID_SERVICE: 'http://localhost:8119/v1/',
-  SGA_MID_SERVICE: "http://pruebasapi.intranetoas.udistrital.edu.co:8119/v1/",
   DOCUMENTO_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/documento_crud/v2/',
-  SGA_PRACTICA_ACADEMICA_MID: 'http://localhost:8096/v1/',
+  SGA_PRACTICA_ACADEMICA_MID: 'http://pruebasapi3.intranetoas.udistrital.edu.co:8541/v1/',
 };
