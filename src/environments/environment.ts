5bbd4b0adb0a3cfff4e1f6aeb449d61e3a7f3495
Chopperkasa <limaszandresfelipe@gmail.com>
2024-06-24 01:24:43 -0500

    feat: udistrital/sga_documentacion#311 Modificación del lugar de almacenamiento del secret key para encriptar

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   urlMf: "http://localhost:4210/",
+  encryptSecretKey: process.env["NG_APP_KEY"],
   NUXEO_SERVICE: "http://pruebasapi2.intranetoas.udistrital.edu.co:8199/v1",
   TERCEROS_SERVICE: 'http://pruebasapi.intranetoas.udistrital.edu.co:8121/v1/',
   DOCUMENTO_SERVICE: 'http://pruebasapi.intranetoas.udistrital.edu.co:8094/v1/',
